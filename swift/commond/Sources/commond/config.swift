e2c7550b0d5737020b348838054a218c8cffe456
abliger <3330181534@qq.com>
2022-03-09 00:31:08 +0800

    format code ;修复逻辑错误； 添加条件 不能重复左右滑动

--- swift/commond/Sources/commond/config.swift
+++ swift/commond/Sources/commond/config.swift
@@ -5,20 +5,19 @@
 //  Created by 冯思学 on 2022/3/6.
 //
 
-import Foundation
-
-public enum D:String{
-    case R,L
+public enum D: String {
+    case R, L
 }
 
-public struct Config{
+public struct Config {
     /// 数组长度 调整此值 显示的列数会跟随变化
-    public static var length:Int=10
+    public static var length: Int = 4
     /// 数组宽度 调整此值 显示的行数会跟随变化
-    public static var width:Int=4
-    public static var dimension:Dimension = .dimension
+    public static var width: Int = 4
+    public static var dimension: Dimension = .dimension
+    public static var score: Int = 0
 }
 
-public enum Dimension{
-    case dimension,two_dimension
+public enum Dimension {
+    case dimension, two_dimension
 }
--- swift/commond/Sources/commond/main.swift
+++ swift/commond/Sources/commond/main.swift
@@ -1,82 +1,107 @@
 import Darwin
+
 /// 操作数组对 当前数组进行合并数字
-public func runable(_ t:inout [Int],_ d: D){
-    switch d{
+public func runable(_ t: inout [Int], _ d: D) -> Bool {
+    var flag: Bool
+    switch d {
     case .L:
-        merge(&t)
-        move(&t)
+        flag = merge(&t)
+        flag = move(&t) || flag
     case .R:
-        var z:[Int]=t.reversed()
-        merge(&z)
-        move(&z)
-        t=z.reversed()
+        flag = merge(&t)
+        var z: [Int] = t.reversed()
+        flag = move(&z) || flag
+        t = z.reversed()
     }
+    return flag
 }
+
 /// 合并函数 [4,0,2,2] -> [4,0,4,0] [2,0,0,2]->[4,0,0,0]
-public func merge(_ t:inout [Int]){
-    for var i in 0 ..< t.count - 1 {
-        for j in i+1 ... t.count - 1 {
-            if(t[i] != 0 && t[j] != 0 && t[i] != t[j]){
+public func merge(_ t: inout [Int]) -> Bool {
+    var flag = false
+    for var i in 0..<t.count - 1 {
+        for j in i + 1...t.count - 1 {
+            if (t[i] != 0 && t[j] != 0 && t[i] != t[j]) {
                 break
             }
-            if(t[i] != 0 && t[j] != 0 && t[i]==t[j]){
-                    t[i]<<=1
-                    t[j]=0
-                    i+=j-i
+            if (t[i] != 0 && t[j] != 0 && t[i] == t[j]) {
+                Config.score += t[i]
+                t[i] <<= 1
+                t[j] = 0
+                i += j - i
+                flag = true
                 break
             }
         }
     }
+    return flag
 }
 
 /// 数字移动 [4,0,2,2] -> [4,2,2,0] [2,0,0,2]->[2,2,0,0]
-public func move(_ t:inout [Int]){
-    var arr=[Int](repeating: 0, count: t.count)
-    var num=0
-    for i in 0 ..< t.count{ 
-        if(t[i] != 0){
-            arr[num]=t[i]
-            num+=1
+public func move(_ t: inout [Int]) -> Bool {
+    var flag = false
+    for i in 0..<t.count - 1 {
+        if (t[i] == 0) {
+            for j in i + 1..<t.count {
+                if (t[j] != 0) {
+                    var temp = t[j]
+                    t[j] = t[i]
+                    t[i] = temp
+                    flag = true
+                    break
+                }
+            }
         }
     }
-    t=arr
+    return flag
 }
 
-func check(_ t:[Int])-> Bool{
-    let temp = t.filter({ $0 == 0 })
-    for i in 0 ..< temp.count - 1{
-        if(temp[i]==temp[i+1]){
+func check(_ t: [Int]) -> Bool {
+    let temp = t.filter({ $0 != 0 })
+    if temp.count < Config.length {
+        return false
+    }
+    for i in 0..<temp.count - 1 {
+        if (temp[i] == temp[i + 1]) {
             return false
         }
     }
+    print(list, "  \(Config.score)")
     return true
 }
+
 /// 从t 为0 的位置随机插入新值
-func randomInsert(_ t:inout [Int]){
-    let num=Int.random(in: 0..<100) > 25 ? 2:4
-    var l=[Int]()
-    for i in 0 ... t.count - 1{
-        if(t[i]==0){
+func randomInsert(_ t: inout [Int]) {
+    let num = Int.random(in: 0..<100) > 25 ? 2 : 4
+    var l = [Int]()
+    for i in 0...t.count - 1 {
+        if (t[i] == 0) {
             l.append(i)
         }
     }
-    t[l[Int.random(in: 0..<l.count)]]=num
+    t[l[Int.random(in: 0..<l.count)]] = num
 }
 
-var list=[Int](repeating: 0, count: Config.length)
+var list = [Int](repeating: 0, count: Config.length)
 
 randomInsert(&list)
 print(list)
-while true{
+while true {
     if let str = readLine()?.uppercased() {
-        if let e = D(rawValue: str){
-            runable(&list,e)
-            randomInsert(&list)
-            print(list)
-            if(check(list)){
+        if let e = D(rawValue: str) {
+            if (runable(&list, e)) {
+                randomInsert(&list)
+            } else {
+                print("不能向 \(e) 移动了")
+            }
+
+            if (check(list)) {
+                print("你输了！")
                 exit(0)
             }
-        }else if str == "QUIT"{
+            print(list, "  \(Config.score)")
+        } else if str == "QUIT" {
+            print("退出游戏")
             exit(0)
         }
     }
